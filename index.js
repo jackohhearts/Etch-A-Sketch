1637ecefc2bc32af02ca45d8aafc6a2729d5a3c3
Joseph Campbell <thecanadianjoe@gmail.com>
2024-04-04 14:44:14 -0400

    drawing ability
    added ability to draw with mouse hover

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+const container = document.getElementById("container")
+
+
+let mouseDown = false;
+document.body.onmouseover = function() {
+  mouseDown = true;
+};
+document.body.onmouseup = function() {
+  mouseDown = false;
+};
+
 for(i = 0; i <= 256; i++){
-let div = document.createElement("div")
-div.setAttribute("id", "Div"+1)
-document.getElementById("container").appendChild(div)
+const cell = document.createElement("div")
+cell.addEventListener('mouseover', changeColor)
+cell.addEventListener('mousedown', changeColor)
+document.getElementById("container").appendChild(cell)
+container.appendChild(cell).className = 'grid-items'
 }
 
 
+function changeColor(e) {
+    if (mouseDown) {
+    e.target.style.backgroundColor = "black"
+    }
+}
