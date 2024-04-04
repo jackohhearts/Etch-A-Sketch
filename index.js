f5f2d2826f75337f47961f26d7077b161c0f7300
Joseph Campbell <thecanadianjoe@gmail.com>
2024-04-04 15:18:28 -0400

    size button
    working size button, does not clear yet

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
 const container = document.getElementById("container")
+const button = document.getElementById("gridSize-btn")
+let size = ""
 
 
-let mouseDown = false;
+let mouseDown = false
 document.body.onmouseover = function() {
-  mouseDown = true;
-};
-document.body.onmouseup = function() {
-  mouseDown = false;
-};
-
-for(i = 0; i <= 256; i++){
-const cell = document.createElement("div")
-cell.addEventListener('mouseover', changeColor)
-cell.addEventListener('mousedown', changeColor)
-document.getElementById("container").appendChild(cell)
-container.appendChild(cell).className = 'grid-items'
+  mouseDown = true
 }
 
+document.body.onmouseup = function() {
+  mouseDown = false
+}
 
-function changeColor(e) {
-    if (mouseDown) {
-    e.target.style.backgroundColor = "black"
+    button.addEventListener("click", function() {
+        size = prompt("What size would you like your grid to be?")
+        checkSize(size)
+    }
+    )
+
+    function canvas(size) {
+        container.style.setProperty('--grid-column', size);
+        container.style.setProperty('--grid-row', size);
+
+    for(i = 0; i < size * size; i++){
+        const cell = document.createElement("div")
+        cell.addEventListener('mouseover', changeColor)
+        cell.addEventListener('mousedown', changeColor)
+        document.getElementById("container").appendChild(cell)
+        container.appendChild(cell).className = 'grid-items'
     }
 }
+
+    function checkSize(e) {
+        if (e > 100) {
+            alert("That's Too Large! Please Select A Value Less Than 100")
+            canvas(16)
+
+        } else {
+            canvas(e)
+        }
+    }
+
+
+    function changeColor(e) {
+        if (mouseDown) {
+        e.target.style.backgroundColor = "black"
+        }
+    }
+
+ canvas(16)
+    
